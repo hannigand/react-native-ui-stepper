d450fef29e195740c8a57d6b454a9b547353db61
Dan Hannigan <hannigan.d92@gmail.com>
2017-08-02 17:47:36 +0100

    Adding wraps

--- UIStepper.js
+++ UIStepper.js
@@ -1,199 +1,58 @@
-import React, {Component} from 'react';
-import {View, StyleSheet, TouchableOpacity, Image} from 'react-native';
-import PropTypes from 'prop-types';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
 
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-  },
-  button: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import UIStepper from 'react-native-ui-stepper';
 
-class UIStepper extends Component {
-  static propTypes = {
-    initialValue: PropTypes.number,
-    minimumValue: PropTypes.number,
-    maximumValue: PropTypes.number,
-    steps: PropTypes.number,
-    tintColor: PropTypes.string,
-    backgroundColor: PropTypes.string,
-    decrementImage: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-    incrementImage: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-    imageWidth: PropTypes.number,
-    imageHeight: PropTypes.number,
-    width: PropTypes.number,
-    height: PropTypes.number,
-    borderColor: PropTypes.string,
-    borderWidth: PropTypes.number,
-    borderRadius: PropTypes.number,
-    onValueChange: PropTypes.func,
-    onDecrement: PropTypes.func,
-    onIncrement: PropTypes.func,
-    onMinimumReached: PropTypes.func,
-    onMaximumReached: PropTypes.func,
-    wraps: PropTypes.bool,
-  };
-  static defaultProps = {
-    initialValue: 0,
-    minimumValue: 0,
-    maximumValue: 100,
-    steps: 1,
-    tintColor: '#0076FF',
-    backgroundColor: 'transparent',
-    decrementImage: require('./assets/decrement.png'),
-    incrementImage: require('./assets/increment.png'),
-    imageWidth: 45,
-    imageHeight: 27,
-    width: 94,
-    height: 29,
-    borderColor: '#0076FF',
-    borderWidth: 1,
-    borderRadius: 4,
-    onValueChange: null,
-    onDecrement: null,
-    onIncrement: null,
-    onMinimumReached: null,
-    onMaximumReached: null,
-    wraps: false,
-  };
-  constructor(props) {
-    super(props);
+export default class test extends Component {
+  constructor() {
+    super();
     this.state = {
-      value: this.props.initialValue,
-    };
+      counter: 0
+    }
   }
-  decrement = () => {
-    const {
-      steps,
-      onDecrement,
-    } = this.props;
-    let value = this.state.value;
-    value -= steps;
-    this.validate(value, onDecrement);
-  };
-  increment = () => {
-    const {
-      steps,
-      onIncrement,
-    } = this.props;
-    let value = this.state.value;
-    value += steps;
-    this.validate(value, onIncrement);
-  };
-  isExternalImage = image => typeof image === 'string';
-  resolveImage = image => {
-    if (this.isExternalImage(image)) {
-      return {uri: image};
-    }
-    return image;
-  };
-  resolveStyles = image => {
-    const {
-      tintColor,
-      height,
-      width,
-    } = this.props;
-    if (this.isExternalImage(image)) {
-      return {
-        height: (height) - 10,
-        width: (width / 2) - 10,
-      };
-    }
-    return {
-      tintColor,
-    };
-  };
-  validate = (value, callback) => {
-    const {
-      minimumValue: min,
-      maximumValue: max,
-      onValueChange,
-      onMinimumReached,
-      onMaximumReached,
-      wraps,
-    } = this.props;
-    if (min <= value && max >= value) {
-      this.setState({
-        value,
-      });
-      if (onValueChange) {
-        onValueChange(value);
-      }
-      if (callback) {
-        callback(value);
-      }
-    }
-    if (onMinimumReached && min === value) {
-      onMinimumReached(value);
-    }
-    if (onMaximumReached && max === value) {
-      onMaximumReached(value);
-    }
-  };
+  updateCounter = counter => {
+    this.setState({
+      counter,
+    })
+  }
   render() {
-    const {
-      tintColor,
-      backgroundColor,
-      decrementImage,
-      dividerImage,
-      incrementImage,
-      imageWidth,
-      imageHeight,
-      width,
-      height,
-      borderColor,
-      borderWidth,
-      borderRadius,
-    } = this.props;
     return (
-      <View
-        style={[
-          styles.container,
-          {
-            backgroundColor,
-            width,
-            height,
-            borderColor,
-            borderWidth,
-            borderRadius,
-          },
-        ]}
-      >
-        <TouchableOpacity
-          onPress={() => {
-            this.decrement();
-          }}
-          style={[
-            styles.button,
-            {
-              borderRightWidth: borderWidth,
-              borderRightColor: borderColor,
-            },
-          ]}
-        >
-          <Image
-            source={this.resolveImage(decrementImage)}
-            style={[this.resolveStyles(decrementImage)]}
+      <View style={styles.container}>
+        <Text> {this.state.counter}</Text>
+        <View style={styles.stepperContainer}>
+          <UIStepper
+            onValueChange={(value) => { this.updateCounter(value) }}
+            maximumValue={10}
+            wraps={true}
           />
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => {
-            this.increment();
-          }}
-          style={styles.button}
-        >
-          <Image
-            source={this.resolveImage(incrementImage)}
-            style={[this.resolveStyles(incrementImage)]}
-          />
-        </TouchableOpacity>
+        </View>
+        
       </View>
     );
   }
 }
 
-export default UIStepper;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderWidth: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  stepperContainer: {
+    borderWidth: 1,
+    flex: 1,
+  }
+});
+
+AppRegistry.registerComponent('test', () => test);
